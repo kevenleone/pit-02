597c5a2a506237bbc2fefe7276ffd483baa2ea2e
Keven Leone <keven.santos.sz@gmail.com>
2021-02-11 21:21:11 -0300

    Add eslint and bootstrap packages

---
+++ frontend-cra/src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Caixa = ({ title, children }) => (
+  <Card className="mb-4">
+    <Card.Header>
+      <Card.Title>{title}</Card.Title>
+    </Card.Header>
+    <Card.Body>{children}</Card.Body>
+  </Card>
+);
+
+export default Caixa;
