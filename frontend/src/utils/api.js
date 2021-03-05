907df6ab0f47bc6fa698b2d0cdafebd74cfbc2d0
Keven Leone <keven.leone@liferay.com>
2021-03-05 09:45:05 -0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -5,7 +5,7 @@ const { REACT_APP_API_BASE_URL } = process.env;
 const myAxios = axios.create({
   baseURL: REACT_APP_API_BASE_URL,
   headers: {
-    Authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDNlZGU4NmE2ZDZjMGI1OTU4MWJhYmQiLCJuYW1lIjoia2V2ZW4iLCJlbWFpbCI6ImtldmVuQGhvdG1haWwuY29tIiwiY3JlYXRlZEF0IjoiMjAyMS0wMy0wM1QwMDo1NTozNC4xNDhaIiwidXBkYXRlZEF0IjoiMjAyMS0wMy0wM1QwMDo1NTozNC4xNDhaIiwiX192IjowLCJpYXQiOjE2MTQ4OTc3Nzl9.CfLgS5aAjmOyrDhsAPUixebjRUuHS6Oql_4RWOuSyhs',
+    Authorization: 'bearer',
   },
 });
 
